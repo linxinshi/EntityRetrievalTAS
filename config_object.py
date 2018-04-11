e8c3d7f06973206b686be82037ff0be02ed38290
Xinshi Lin <linxinshi1992@gmail.com>
2018-04-11 18:27:37 +0800

    Update config_object.py

--- config_object.py
+++ config_object.py
@@ -5,7 +5,7 @@ import os
 class Config_Object(object):
 
       system_flag=None    
-      mongo_port=58903
+      mongo_port=27017
       
       def __init__(self):
           self.system_flag=SYSTEM_FLAG
@@ -17,11 +17,3 @@ class Config_Object(object):
           self.PATH_GROUNDTRUTH=os.path.join('qrels-v2.txt')
           self.PATH_CATEGORY_DAG='category_dag_dbpedia_top10.pkl.gz'
           
-          if self.system_flag=='Windows':
-             self.LUCENE_INDEX_DIR=os.path.join('E:\\',self.LUCENE_INDEX_DIR)
-             self.LUCENE_INDEX_WIKI_DIR=os.path.join('E:\\',self.LUCENE_INDEX_WIKI_DIR)
-             self.LUCENE_INDEX_CATEGORY_CORPUS=os.path.join('E:\\',self.LUCENE_INDEX_CATEGORY_CORPUS)
-             self.QUERY_FILEPATH=os.path.join('E:\\','Entity_Retrieval',self.QUERY_FILEPATH)
-             self.PATH_GROUNDTRUTH=os.path.join('E:\\','Entity_Retrieval','Balog_SIGIR13',self.PATH_GROUNDTRUTH)
-             self.PATH_CATEGORY_DAG='F:\\研究数据\\Wikipedia_DBpedia_data\\DBpedia_data\\2015-10\\category_structure_processing\\category_dag_dbpedia_top10.pkl.gz'
-             self.mongo_port=27017
